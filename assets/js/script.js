a9563ae4be741132046736ddb33035ef4275ec7a
Madebybrown <madebybrown@gmail.com>
2022-09-20 09:51:01 +0000

    Add query selector for game choice buttons, add eventlistener to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
-let computer = document.getElementById("computer")
-let player = document.getElementById("player")
-let result = document.getElementById("result")
+// Game Area
+let computer = document.getElementById('computer')
+let player = document.getElementById('player')
+let result = document.getElementById('result')
+
+// Game Choices
+let choices = document.querySelectorAll('button')
+let usersChoice
+
+choices.forEach(choice => choices.addEventListener('click', (event) => {
+    usersChoice = event.target.id
+}))
