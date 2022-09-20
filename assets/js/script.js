50c2c3e5af312c8b20ac6dd6a957a58cbe982cdf
Madebybrown <madebybrown@gmail.com>
2022-09-20 10:18:59 +0000

    Add basic script logic to computer choices and display inner html of cumputers choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,8 @@ let result = document.getElementById('result')
 // Game Choices
 let choices = document.querySelectorAll('button')
 let playerChoice
+let computerChoice
+
 /**
  * Listens for clicks on each individual button and displays the 
  * inner html of the button that was clicked in the player field
@@ -13,4 +15,21 @@ let playerChoice
 choices.forEach(choice => choice.addEventListener('click', (event) => {
     playerChoice = event.target.id
     player.innerHTML = playerChoice
+    randomComputerChoice()
 }))
+
+//  
+function randomComputerChoice() {
+    let randomChoice = Math.floor(Math.random() * 3)
+    
+    if (randomChoice === 0) {
+        computerChoice = 'rock'
+    }
+    if (randomChoice === 1) {
+        computerChoice = 'paper'
+    }
+    if (randomChoice === 2) {
+        computerChoice = 'scissors'
+    }
+    computer.innerHTML = computerChoice
+}
