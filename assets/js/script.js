8b167d6e8665be891dfb75211e9e860bdd6ac7be
Madebybrown <madebybrown@gmail.com>
2022-09-20 10:04:42 +0000

    Add description to script

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,10 @@ let result = document.getElementById('result')
 // Game Choices
 let choices = document.querySelectorAll('button')
 let playerChoice
-// Listens for clicks on each individual button
+/**
+ * Listens for clicks on each individual button and displays the 
+ * inner html of the button that was clicked in the player field
+ * */
 choices.forEach(choice => choice.addEventListener('click', (event) => {
     playerChoice = event.target.id
     player.innerHTML = playerChoice
